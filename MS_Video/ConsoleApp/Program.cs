a5c775b74b65125f036271605c5af3b1556fed4f
Craig Totel <craig.totel@gmail.com>
2017-06-17 07:38:08 -0600

    Updates

---
+++ MS_Video/ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Video1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   string name;
+		
+            Console.WriteLine("Hello World!");
+			
+			name = Console.ReadLine();
+			  // C# 7
+			Console.WriteLine ($"Hello {name}");
+			Console.WriteLine ("Hello2 " +name);
+			
+        }
+    }
+}
---
+++ MS_Video/MakeWeb/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using movingtoweb;
+
+namespace Video1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+		    var host = new WebHostBuilder()
+			    .UseKestrel()
+				.UseStartup<Startup>()
+				.Build();
+				
+				host.Run();
+        }
+    }
+}
