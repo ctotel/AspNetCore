ce8bd20d3384686140001dd74020ee346055080a
Craig Totel <craig.totel@gmail.com>
2017-06-19 12:40:03 -0600

    Added test

---
+++ MS_Video/Video4/WeppApp/Models/Album.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeppApp.Models
+{
+    public class Album
+    {
+        public int ID { get; set; }
+        public string Artist { get; set; }
+        public string Name { get; set; }
+        public string Gene { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public decimal Price { get; set; }
+    }
+}
